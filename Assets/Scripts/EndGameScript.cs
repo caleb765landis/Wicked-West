a39348572599298ac515c6081eafa0b1f0d4888f
caleb765landis <caleb765landis@gmail.com>
2023-04-28 21:27:45 -0400

    End game triggers

--- Assets/Scripts/EndGameScript.cs
+++ Assets/Scripts/EndGameScript.cs
@@ -1,15 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGameScript : MonoBehaviour
 {
     public Transform player;
     public GameObject EndGameUI;
+    public bool doRestart = true;
+    public float fadeDuration = 3f;
+    public float displayImageDuration = 3f;
+    public float m_Timer = 0f;
+
+    public bool endLevel = false;
 
     private void Awake()
     {
         player = GameObject.Find("PlayerModels").transform;
+    }
+
+    void Update() {
+        if (endLevel) {
+            m_Timer += Time.deltaTime;
+
+            if (m_Timer > fadeDuration + displayImageDuration)
+            {
+                if (doRestart)
+                {
+                    SceneManager.LoadScene (2);
+                }
+                else
+                {
+                    Application.Quit ();
+                }
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -19,6 +44,10 @@ public class EndGameScript : MonoBehaviour
 			player.GetComponent<PlayerController>().isDead = true;
 			player.GetComponent<PlayerController>().playeranim.SetBool("isWalking", false);
             EndGameUI.SetActive(true);
+
+            endLevel = true;
+
+
 		}
     }
 }
